527a50545dd688fa51d12ec3aa571d2c2377ace6
williamle92 <william1992le@gmail.com>
2021-06-18 00:19:29 -0700

    fixed the grid structure for the dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -2,38 +2,48 @@ import React, { Component } from "react";
 import TodoTracker from "../components/TodoTracker";
 import Grid from "@material-ui/core/Grid";
 import Sidebar from "../components/Sidebar";
-
+import "./Dashboard.css";
 import Calendar from "react-calendar";
 
 export default class Dashboard extends Component {
-  state = { affirmation : ''}
-
-  componentDidMount(){
-    fetch('https://www.affirmations.dev/')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-   
-      
-    })
+  constructor() {
+    super();
+    this.state = {
+      affirmation: "",
+    };
   }
 
-
+  componentDidMount() {
+    fetch("https://www.affirmations.dev/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          affirmation: data.affirmation,
+        });
+      });
+  }
 
   render() {
     return (
       <div>
-        <Grid container>
+        <Grid container spacing={1}>
           <Grid item xs={2}>
             <Sidebar />
           </Grid>
+          <Grid item xs={5} alignItems="center" justify="center">
+            <h1 className="affirm">{this.state.affirmation}</h1>
+          </Grid>
           <Grid item xs={5}>
-            <Calendar />
+            <div className="calendar">
+              <Calendar />
+            </div>
           </Grid>
-          <Grid item xs={10}>
-          <TodoTracker />
+          <Grid item xs={10} spacing={9}>
+            <div className="todo">
+              <TodoTracker />
+            </div>
           </Grid>
-
         </Grid>
       </div>
     );
